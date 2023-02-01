776bb19f8636336853e5494f8fcd73b880dcb3d4
silence_zhpf <silence_zhpf@aliyun.com>
2023-02-01 15:17:08 +0800

    feat: ✨ 添加4的幂的O(1)算法实现

--- src/is_power_of_four.rs
+++ src/is_power_of_four.rs
@@ -1,9 +1,9 @@
 //! 342. 4的幂
 //!
 //! 给定一个整数，写一个函数来判断它是否是 4 的幂次方。如果是，返回 true ；否则，返回 false 。
-//
+//!
 //! 整数 n 是 4 的幂次方需满足：存在整数 x 使得 n == 4x
-//
+//!
 //! 来源：力扣（LeetCode）
 //! 链接：https://leetcode.cn/problems/power-of-four
 
@@ -26,19 +26,9 @@ mod test {
     }
 }
 
+/// 根据4的幂的二进制特点求解：
+/// 1. 只有1个1
+/// 2. 1之后的0的个数为偶数
 pub fn is_power_of_four(n: i32) -> bool {
-    if n < 1 {
-        return false;
-    }
-    if n == 1 {
-        return true;
-    }
-    let mut n = n;
-    while n != 4 {
-        if n % 4 != 0 {
-            return false;
-        }
-        n /= 4;
-    }
-    true
+    n > 0 && n.count_ones() == 1 && n.trailing_zeros() % 2 == 0
 }
